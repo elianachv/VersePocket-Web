aa7914f450455e67693123feba7f3baaef8b6309
Eliana <elianachavezv@gmail.com>
2021-02-02 16:00:46 -0500

    Correcion del repositorio

--- BACKEND/src/controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-function mostrarSitioWeb(req, res) {
-    res.send('Página principal de la aplicación por ahora');
-    //res.render('index', {layout: 'main'});
-}
-
-module.exports = {
-    mostrarSitioWeb
-}
--- BACKEND/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const indexController = require('../controllers/indexController');
-
-router.get('/', indexController.mostrarSitioWeb);
-
-module.exports = router;
